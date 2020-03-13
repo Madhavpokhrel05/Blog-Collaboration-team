bd05b95adfbef119c1b83a9e101aa25aaad65fb3
Madhav Pokhrel <saraswatiacharya05@gmail.com>
2020-03-13 16:55:52 -0400

    added Hellow world

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
 
+app.get('/', (req, res) => {
+  res.send('Hellow World!')
+})
 
 app.listen(PORT, ()=> {
     console.log('Listening')
