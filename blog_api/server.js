e702b0e11aab71776ae0edba6fca85b29bfef292
Madhav Pokhrel <saraswatiacharya05@gmail.com>
2020-03-14 14:44:06 -0400

    added whitelist in blog_api and middleware

--- blog_api/server.js
+++ blog_api/server.js
@@ -7,6 +7,18 @@ const blogsController = require('./controllers/blogs.js')
 
 app.use(express.json())
 
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
